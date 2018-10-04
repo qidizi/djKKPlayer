9c488a1584c9f3830656adf99e9e2ea67d32a5f4
qidizi <qidizi@qq.com>
2018-10-05 03:15:46 +0800

    f

--- insert.js
+++ insert.js
@@ -230,8 +230,8 @@ function javascript() {
         return '<li class="list-group-item ' + (cls || '') + ' text-right"  data-href="' + src + '">' +
             '<a class="btn btn-link jsPlayMe jsSongTitle" href="javascript:void(0);">' +
             title +
-            '</a> ' +
-            '<a class="btn btn-outline-danger jsRemoveMe" href="javascript:void(0);">删除</a> ' +
+            '</a>' +
+            ' <a class="btn btn-outline-danger jsRemoveMe" href="javascript:void(0);">删除</a> ' +
             '</li>';
     }
 
@@ -325,8 +325,8 @@ function javascript() {
     function resultLi(id, title) {
         return '<li class="list-group-item text-right"  data-href="' + source + '/play/' + id + '.html">' +
             '<a href="javascript:void(0)" class="btn btn-link jsPlayResultItem jsSongTitle" data-is="play">' + title + '</a>' +
-            '<a href="javascript:void(0)" class="btn btn-outline-primary jsPlayResultItem" data-is="test">试听</a>' +
-            '<a href="javascript:void(0)" class="btn btn-outline-success jsPlayResultItem" data-is="append">加入</a>' +
+            ' <a href="javascript:void(0)" class="btn btn-outline-primary jsPlayResultItem" data-is="test">试听</a>' +
+            ' <a href="javascript:void(0)" class="btn btn-outline-success jsPlayResultItem" data-is="append">加入</a>' +
             '</li>';
     }
 
